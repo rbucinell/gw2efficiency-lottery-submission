a386900cc2c86165dd39a14490086299aa9ad371
Bucinell, Ryan M <rmbucinell@paychex.com>
2021-08-11 15:57:13 -0400

    Added script logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 const pw = require('playwright');
+require('dotenv').config();
+
+(async ()=>{
+
+    //Setup Browswer
+    const browser = await pw[process.env.DRIVER].launch();
+    const context = await browser.newContext();
+    const page    = await context.newPage();
+
+    //Do work
+    await page.goto('https://gw2efficiency.com/community/lottery');
+    await page.fill('input[type="text"]', process.env.GW2ACCT);
+    await page.click('.enter-button');
+    
+    //Close when finished
+    await browser.close();
+})();
